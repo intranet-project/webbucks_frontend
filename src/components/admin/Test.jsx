467c14b8bbc237c4883e013d482b78745974b0b9
wch <wonchiho9@gamil.com>
2024-06-27 21:29:36 +0900

    feat: Route test

---
+++ src/components/admin/Test.jsx
@@ -0,0 +1,9 @@
+const Test = () => {
+  return (
+    <div>
+      <p>테스트~</p>
+    </div>
+  );
+};
+
+export default Test;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -10,3 +11,4 @@ root.render(
     <App />
   </BrowserRouter>
 );
+reportWebVitals();
