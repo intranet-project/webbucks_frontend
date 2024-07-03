b3563a9239d1f3937f091160aa69ca0e22331790
wch <wonchiho9@gamil.com>
2024-07-03 16:11:57 +0900

    fix: 메뉴관리, 오더관리 수정 및 Modal창 구현

--- src/components/admin/AdminMenu.jsx
+++ src/components/admin/AdminMenu.jsx
@@ -6,8 +6,15 @@ const AdminMenu = () => {
   const [apiData, setApiData] = useState([]);
 
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const openModal = () => setIsModalOpen(true);
-  const closeModal = () => setIsModalOpen(false);
+  const [selectedData, setSelectedData] = useState({});
+  const openModal = (menuData) => {
+    setSelectedData(menuData);
+    setIsModalOpen(true);
+  };
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setSelectedData({});
+  };
 
   useEffect(() => {
     getApi();
@@ -31,6 +38,7 @@ const AdminMenu = () => {
       })
       .then((menu) => {
         console.log(menu);
+        setSelectedData(menu.data);
         getApi();
       })
       .catch((error) => console.log(error));
@@ -40,7 +48,14 @@ const AdminMenu = () => {
     <div>
       {apiData.map((apiData, index) => (
         <div key={index}>
-          <span>{`${index + 1}. 메뉴명: ${apiData.menuName}, `}</span>
+          <span
+            onClick={() => openModal(apiData)}
+            style={{
+              cursor: "pointer",
+              textDecoration: "underline",
+              color: "blue",
+            }}
+          >{`${index + 1}. 메뉴명: ${apiData.menuName}, `}</span>
           <span>{` 메뉴이미지 : ${apiData.menuImg}`}</span>
           <span>{` 가격 : ${apiData.menuPrice}`}</span>
           <span>{` 카테고리 : ${apiData.categoryname}`}</span>
@@ -53,10 +68,26 @@ const AdminMenu = () => {
         </div>
       ))}
 
-      <button onClick={openModal}>모달</button>
       <ModalForm isOpen={isModalOpen} closeModal={closeModal}>
-        <h2>hi</h2>
-        <p>it's me</p>
+        <span>{`메뉴명: ${selectedData.menuName}, `}</span>
+        <br />
+        <span>{`메뉴이미지 : ${selectedData.menuImg}`}</span>
+        <br />
+        <span>{`메뉴설명 : ${selectedData.menuDetail}`}</span>
+        <br />
+        <span>{`가격 : ${selectedData.menuPrice}`}</span>
+        <br />
+        <span>{`카테고리 : ${selectedData.categoryname}`}</span>
+        <br />
+        <span>{`품절여부 : ${selectedData.soldoutState}, `}</span>
+        <button onClick={() => updateApi(selectedData.menuId, "Y")}>
+          품절
+        </button>
+        <button onClick={() => updateApi(selectedData.menuId, "N")}>
+          품절취소
+        </button>
+        <br />
+        <button onClick={closeModal}>닫기</button>
       </ModalForm>
     </div>
   );
--- src/components/admin/AdminOrder.jsx
+++ src/components/admin/AdminOrder.jsx
@@ -1,8 +1,20 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import ModalForm from "../common/ModalForm";
 
 const AdminOrder = () => {
   const [apiData, setApiData] = useState([]);
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedData, setSelectedData] = useState({});
+  const openModal = (menuData) => {
+    setSelectedData(menuData);
+    setIsModalOpen(true);
+  };
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setSelectedData({});
+  };
 
   useEffect(() => {
     getApi();
@@ -29,6 +41,7 @@ const AdminOrder = () => {
       )
       .then((order) => {
         console.log(order);
+        setSelectedData(order.data);
         getApi();
       })
       .catch((error) => console.log(error));
@@ -38,27 +51,66 @@ const AdminOrder = () => {
     <div>
       {apiData.map((apiData, index) => (
         <div key={index}>
-          <span>{`${index + 1}.주문번호: ${apiData.b_orderId}, `}</span>
+          <span
+            onClick={() => openModal(apiData)}
+            style={{
+              cursor: "pointer",
+              textDecoration: "underline",
+              color: "blue",
+            }}
+          >{`${index + 1}.주문번호: ${apiData.b_orderId}, `}</span>
           <span>{` 고객ID : ${apiData.custId}`}</span>
           <span>{` 메뉴 : ${apiData.menuName}`}</span>
           <span>{` 주문시간 : ${apiData.b_orderCreatedAt}`}</span>
           <span>{` 완료시간 : ${apiData.b_orderStateUpdateAt}`}</span>
-          <br />
           <span>{` 오더상태 : ${apiData.b_orderState}`}</span>
+          <br />
           <button onClick={() => updateApi(apiData.b_orderStatusId, "대기")}>
-            1
+            대기
           </button>
           <button onClick={() => updateApi(apiData.b_orderStatusId, "준비중")}>
-            2
+            준비중
           </button>
           <button onClick={() => updateApi(apiData.b_orderStatusId, "완료")}>
-            3
+            완료
           </button>
           <button onClick={() => updateApi(apiData.b_orderStatusId, "취소")}>
-            4
+            취소
           </button>
+          <br />
         </div>
       ))}
+
+      <ModalForm isOpen={isModalOpen} closeModal={closeModal}>
+        <span>{`주문번호: ${selectedData.b_orderId}, `}</span>
+        <br />
+        <span>{`고객ID : ${selectedData.custId}`}</span>
+        <br />
+        <span>{`메뉴 : ${selectedData.menuName}`}</span>
+        <br />
+        <span>{`주문시간 : ${selectedData.b_orderCreatedAt}`}</span>
+        <br />
+        <span>{`완료시간 : ${selectedData.b_orderStateUpdateAt}`}</span>
+        <br />
+        <span>{` 오더상태 : ${selectedData.b_orderState}`}</span>
+        <br />
+        <button onClick={() => updateApi(selectedData.b_orderStatusId, "대기")}>
+          대기
+        </button>
+        <button
+          onClick={() => updateApi(selectedData.b_orderStatusId, "준비중")}
+        >
+          준비중
+        </button>
+        <button onClick={() => updateApi(selectedData.b_orderStatusId, "완료")}>
+          완료
+        </button>
+        <button onClick={() => updateApi(selectedData.b_orderStatusId, "취소")}>
+          취소
+        </button>
+        <br />
+        <button onClick={closeModal}>닫기</button>
+      </ModalForm>
     </div>
   );
 };
